e58084017f802414f46d4ebf6468aa784aa0427f
Juan Escalada <juanescalada175@gmail.com>
2023-10-05 16:46:17 -0700

    Add deadlines to listener

--- app/src/main/java/com/example/deadlinebot/Main.kt
+++ app/src/main/java/com/example/deadlinebot/Main.kt
@@ -4,27 +4,23 @@ import dev.kord.core.Kord
 import dev.kord.core.exception.KordInitializationException
 import dev.kord.gateway.Intent
 import dev.kord.gateway.PrivilegedIntent
-import io.github.cdimascio.dotenv.dotenv
 
 suspend fun main() {
-    // Get bot token from .env
-    val dotenv = dotenv {
-        directory = "app/assets"
-        filename = "env"
-    }
-    val token = dotenv["BOT_TOKEN"]
+    val token = DataHelper.loadBotTokenFromEnv("app/assets", "env")
 
     // Initialize bot
     val kord: Kord
     try {
-        kord = Kord(token ?: "")
+        kord = Kord(token)
     } catch (e: KordInitializationException) {
         println("Invalid token. Please check your env file at app/assets/env.")
         return
     }
 
+    val deadlines = DataHelper.loadDeadlinesFromFile("app/assets/deadlines.txt")
+
     // Register event listeners
-    ListenerHelper.onMessageCreateEvent(kord)
+    ListenerHelper.onMessageCreateEvent(kord, deadlines)
 
     // Start bot (log it in)
     kord.login {
